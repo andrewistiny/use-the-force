bc9955b6c6cacacd5261a47dccf01fa96dd787c5
andrewistiny <aleachman3619@myhta.org>
2018-05-17 14:39:00 -1000

    complete

--- app.js
+++ app.js
@@ -37,4 +37,48 @@ function reqListener3 () {
     oReq3.open("GET", "https://swapi.co/api/people/4/?response=height");       
     oReq3.send();
 
-    for(let i = 0; i > data.planets)
+    var oReq4 = new XMLHttpRequest();
+
+
+    function reqListener4 () {
+      var data = JSON.parse(this.responseText);
+      var planets = document.getElementById("planetsResult");
+
+      for(let i = 0; i < 10;i++){
+        console.log(data.results[i].name)
+        var newPlanet = document.createElement("div");
+        newPlanet.innerHTML = data.results[i].name;
+        planets.appendChild(newPlanet); 
+       }
+   }  
+
+   oReq4.addEventListener("load", reqListener4);
+   oReq4.open("GET", "https://swapi.co/api/planets/");       
+   oReq4.send();
+
+
+
+
+
+
+
+   var oReq5 = new XMLHttpRequest();
+
+
+   function reqListener5 () {
+     var data = JSON.parse(this.responseText);
+     var films = document.getElementById("filmsResult");
+
+     for(let i = 0; i < 10;i++){
+       console.log(data.results[i].title)
+       var newfilm = document.createElement("div");
+       newfilm.innerHTML = data.results[i].title;
+       films.appendChild(newfilm); 
+      }
+  }  
+
+  oReq5.addEventListener("load", reqListener5);
+  oReq5.open("GET", "https://swapi.co/api/films/");       
+  oReq5.send();
+
+   
